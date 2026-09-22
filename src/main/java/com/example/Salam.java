37021b3aa9e5f66454e28c5c7c17618e2527abfd
Maxty40 <dhiaudin275@gmail.com>
2026-09-22 13:33:08 +0700

    Initial Commit

---
+++ src/main/java/com/example/Salam.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Salam {
+    public static void main(String x[]) {
+        System.out.print("How Are You? "); // Does not change the line of code
+        System.out.println("Happy Learning Java! "); // Change the line of code 
+        System.out.println("Hope you are doing well! ");
+    }
+}
