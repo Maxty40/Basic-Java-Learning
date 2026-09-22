3f8f828ffd6d1551f5ab18783d43df43980fddd8
Maxty40 <dhiaudin275@gmail.com>
2026-09-22 14:42:10 +0700

    Variable Learning

--- src/main/java/prakpbo/Salam.java
+++ src/main/java/prakpbo/Salam.java
@@ -2,6 +2,26 @@ package prakpbo;
 
 public class Salam {
     public static void main(String x[]) {
+        // String variable
+        String nama = "Ikhsan";
+
+        // Interger variable
+        int umur = 22;
+
+        // Char variable
+        char nilai = 'A';
+
+        // Float variable
+        float phi = 3.142857f;
+
+        // Double variable
+        double phi2 = 3.142857142857143;
+
+        // Print the string and integer variable
+        System.out.println("Halo nama saya " + nama + ", umur saya " + umur + ", nilai saya adalah " + nilai);
+
+        System.out.println("Luas lingkaran adalah " + phi + " Satu lagi " + phi2);
+
         // Print a comment
         System.out.println("Assalamu'alaikum.. ");
         System.out.println("Selamat Belajar Java ");
